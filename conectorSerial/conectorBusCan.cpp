f6e86a34aea14ac8422322ceeb0bfd223c913891
borjabrisson <bbrisson@pamaehijos.com>
2013-08-21 11:25:14 +0100

    update Buscan

--- conectorSerial/conectorBusCan.cpp
+++ conectorSerial/conectorBusCan.cpp
@@ -9,59 +9,61 @@
 
 conectorBusCan::conectorBusCan() {
 	// TODO Auto-generated constructor stub
+	this->port="/dev/";
 	setCommand();
 	setNameCommand();
 }
 
 string conectorBusCan::partialHex(int dec) {
-	string numeroHex="";
-		stringstream stream;
-		if(dec<16) {
-		   switch(dec) {
-				   case 10:
-				   numeroHex="A";
-				   break;
-				   case 11:
-				   numeroHex="B";
-				  break;
-				  case 12:
-				  numeroHex="C";
-				  break;
-				  case 13:
-				  numeroHex="D";
-				  break;
-				  case 14:
-				 numeroHex="E";
-				  break;
-				  case 15:
-				  numeroHex="F";
-				  break;
-				  default:
-					  stream << dec;
-					  numeroHex = stream.str();
-				  break;
-		  }
-		  return numeroHex;
+	string numeroHex = "";
+	stringstream stream;
+	if (dec < 16) {
+		switch (dec) {
+		case 10:
+			numeroHex = "A";
+			break;
+		case 11:
+			numeroHex = "B";
+			break;
+		case 12:
+			numeroHex = "C";
+			break;
+		case 13:
+			numeroHex = "D";
+			break;
+		case 14:
+			numeroHex = "E";
+			break;
+		case 15:
+			numeroHex = "F";
+			break;
+		default:
+			stream << dec;
+			numeroHex = stream.str();
+			break;
 		}
-		return "error";
+		return numeroHex;
+	}
+	return "error";
 }
 
 string conectorBusCan::HexToStr(int dec) {
-	int cociente=16, residuo=0;
-		string numeroHex="", numeroHex1="";
+	int cociente = 16, residuo = 0;
+	string numeroHex = "", numeroHex1 = "";
 
-		while(dec>=16){
-			cociente=dec/16;
-			residuo=dec%16;
-			dec=cociente;
-			numeroHex1=partialHex(residuo);
-			numeroHex=numeroHex1+numeroHex;
-			dec=cociente;
-		}
-		numeroHex1=partialHex(dec);
-		numeroHex=numeroHex1+numeroHex;
-		if (numeroHex.length() == 1) numeroHex = "0"+numeroHex;
-		return numeroHex;
+	while (dec >= 16) {
+		cociente = dec / 16;
+		residuo = dec % 16;
+		dec = cociente;
+		numeroHex1 = partialHex(residuo);
+		numeroHex = numeroHex1 + numeroHex;
+		dec = cociente;
+	}
+	numeroHex1 = partialHex(dec);
+	numeroHex = numeroHex1 + numeroHex;
+	if (numeroHex.length() == 1)
+		numeroHex = "0" + numeroHex;
+	return numeroHex;
 }
 
 conectorBusCan::~conectorBusCan() {
@@ -69,194 +71,135 @@ conectorBusCan::~conectorBusCan() {
 }
 
 void conectorBusCan::setCommand() {
-	commandsID.insert(pair<string,string>("00","TestCtrLink"));
-	commandsID.insert(pair<string,string>("01","Reset"));
-	commandsID.insert(pair<string,string>("02","GetFirmware"));
-	commandsID.insert(pair<string,string>("51","CloseRelay"));
-	commandsID.insert(pair<string,string>("52","SwitchRelay"));
-	commandsID.insert(pair<string,string>("42","SetCFG"));
+	commandsID.insert(pair<string, string>("00", "TestCtrLink"));
+	commandsID.insert(pair<string, string>("01", "Reset"));
+	commandsID.insert(pair<string, string>("02", "GetFirmware"));
+	commandsID.insert(pair<string, string>("51", "CloseRelay"));
+	commandsID.insert(pair<string, string>("52", "SwitchRelay"));
+	commandsID.insert(pair<string, string>("0B", "WrDisplay"));
+	commandsID.insert(pair<string, string>("0C", "WrInmDisplay"));
+	commandsID.insert(pair<string, string>("0A", "ClrDisplay"));
+	commandsID.insert(pair<string, string>("0D", "SaveAndRestoreDisplay"));
+	commandsID.insert(pair<string, string>("5A", "ActiveLed/Buzzer"));
+	commandsID.insert(pair<string, string>("5B", "SwitchLed"));
+	commandsID.insert(pair<string, string>("64", "OutputPort"));
+	commandsID.insert(pair<string, string>("59", "EjectCard"));
+	commandsID.insert(pair<string, string>("60", "CaptureCard"));
+	commandsID.insert(pair<string, string>("42", "SetCFG"));
+	commandsID.insert(pair<string, string>("43", "GetCFG"));
+	commandsID.insert(pair<string, string>("53", "TxDigitalInput"));
 }
 
 void conectorBusCan::setNameCommand() {
-	map<string,string>::iterator it;
-	for(it = commandsID.begin(); it != commandsID.end();it++){
+	map<string, string>::iterator it;
+	for (it = commandsID.begin(); it != commandsID.end(); it++) {
 		commandsName[(*it).second] = (*it).first;
 	}
-	commandsName.insert(pair<string,string>("TestNodeLink","00"));
-	commandsName.insert(pair<string,string>("SetupNCanMax","01"));
+	commandsName.insert(pair<string, string>("TestNodeLink", "00"));
+	commandsName.insert(pair<string, string>("SetupNCanMax", "01"));
 }
 
-string conectorBusCan::calculateCRC(string command){
-	int size = command.size(),amount=0;
-	for(int i=0;i<size; i++){
-		amount += (int)command[i];
+string conectorBusCan::calculateCRC(string command) {
+	int size = command.size(), amount = 0;
+	for (int i = 0; i < size; i++) {
+		amount += (int) command[i];
 	}
 	amount = amount % 256;
 	return this->HexToStr(amount);
 }
 
 int conectorBusCan::getIDCommand(string id) {
-	if (id[0]> '9') id[0] -= 7;
-	if (id[1]> '9') id[1] -= 7;
-	return ((id[0]-48)*16) + (id[1]-48);
+	if (id[0] > '9')
+		id[0] -= 7;
+	if (id[1] > '9')
+		id[1] -= 7;
+	return ((id[0] - 48) * 16) + (id[1] - 48);
 }
-
-
 
 bool conectorBusCan::buildEmptyCmd(string cmd, string node) {
-	string msg =node+cmd+"00";
-		msg += this->calculateCRC(msg);
-	return 	this->sendMessage(msg);
+	string msg = node + cmd + "00";
+	msg += this->calculateCRC(msg);
+	return this->sendMessage(msg);
 }
 
-bool conectorBusCan::buildArgsCmd(string cmd, string node, string args,int minArgs, int maxArgs) {
-	string msg =node+cmd;
-	int size = args.size()/2;
-		if ( (size >= minArgs) && (size <= maxArgs) ){
-			msg += "0/";//+(string)(char)(48+size);
-			msg += args + this->calculateCRC(msg+args);
-	return		this->sendMessage(msg);
-		}
-		return false;
+bool conectorBusCan::buildArgsCmd(string cmd, string node, string args,
+		int minArgs, int maxArgs) {
+	string msg = node + cmd;
+	int size = args.size() / 2;
+	if ((size >= minArgs) && (size <= maxArgs)) {
+		stringstream stream;
+		stream << size;
+		msg += "0"+stream.str(); //+(string)(char)(48+size);
+		msg += args + this->calculateCRC(msg + args);
+		return this->sendMessage(msg);
+	}
+	return false;
 }
 
 bool conectorBusCan::sendMessage(string msg) {
-	string out = '\x02'+msg+'\x03';
-	cout << "Enviamos al bus: "<< out << endl;
+	string out = '\x02' + msg + '\x03';
+	cout << "Enviamos al bus: " << out << endl;
 	return true;
 }
 
 bool conectorBusCan::exec(string command, string node, string args) {
 	string id = commandsName[command];
 
-	cout << "ID: "<< id<< " -- " << getIDCommand(id)<< endl;
-	switch (getIDCommand(id)){
+	cout << "ID: " << id << " -- " << getIDCommand(id) << endl;
+	switch (getIDCommand(id)) {
 	case cm_TestLink:
-		if(command == "TestCtrLink") node="00";
-		this->buildEmptyCmd(id,node);
+		if (command == "TestCtrLink")
+			node = "00";
+		this->buildEmptyCmd(id, node);
 		break;
 	case cm_Reset:
-		this->cmReset(node);
+		this->buildEmptyCmd(id, node);
 		break;
 	case cm_GetFirmware:
-		this->cmGetFirmware(node);
+		this->buildEmptyCmd(id, node);
+		break;
+	case cm_CloseRelay:
+		this->buildArgsCmd(id, node, args, 2, 2);
 		break;
 	case cm_SwitchRelay:
-		cout << "nose"<< endl;
+		this->buildArgsCmd(id, node, args, 2, 2);
+		break;
+	case cm_WrDisplay:
+		this->buildArgsCmd(id, node, args, 1, 6);
+		break;
+	case cm_WrInmDisplay:
+		this->buildArgsCmd(id, node, args, 1, 6);
+		break;
+	case cm_ClrDisplay:
+		this->buildEmptyCmd(id, node);
+		break;
+	case cm_SaveAndRestoreDisplay:
+		this->buildArgsCmd(id, node, args, 1, 1);
+		break;
+	case cm_ActivateLed:
+		this->buildArgsCmd(id, node, args, 2, 2);
+		break;
+	case cm_SwitchLed:
+		this->buildArgsCmd(id, node, args, 2, 2);
+		break;
+	case cm_OutputPort:
+		this->buildArgsCmd(id, node, args, 0, 5);
+		break;
+	case cm_EjectCard:
+		this->buildEmptyCmd(id, node);
+		break;
+	case cm_CaptureCard:
+		this->buildArgsCmd(id, node, args, 6, 6);
 		break;
 	case cm_SetCFG:
-		this->buildArgsCmd(id,node,args,6,6);
+		this->buildArgsCmd(id, node, args, 6, 6);
+		break;
+	case cm_GetCFG:
+		this->buildEmptyCmd(id, node);
+		break;
+	case cm_TxDigitalInput:
+		this->buildEmptyCmd(id, node);
 		break;
 	}
 	return false;
 }
-/*
-void conectorBusCan::cmTestLink(string ID) {
-	string msg =ID+"0000";
-	msg += this->calculateCRC(msg);
-	this->sendMessage(msg);
-}
-
-void conectorBusCan::cmReset(string ID) {
-	string msg =ID+"0100";
-	msg += this->calculateCRC(msg);
-	this->sendMessage(msg);
-}
-
-void conectorBusCan::cmGetFirmware(string ID) {
-	string msg =ID+"0200";
-		msg += this->calculateCRC(msg);
-		this->sendMessage(msg);
-}
-
-void conectorBusCan::cmCloseRelay(string ID, string args) {
-	string msg =ID+"5102";
-	if (args.size() == 4){
-		msg += args + this->calculateCRC(msg+args);
-		this->sendMessage(msg);
-	}
-}
-
-void conectorBusCan::cmSwitchRelay(string ID, string args) {
-	string msg =ID+"5202";
-		if (args.size() == 4){
-			msg += args + this->calculateCRC(msg+args);
-			this->sendMessage(msg);
-		}
-}
-
-void conectorBusCan::cmWrDisplay(string ID, string args) {
-	string msg =ID+"0B";
-	int size = args.size();
-		if ( (size > 2) && (size < 13) ){
-			msg += "0"+size;
-			msg += args + this->calculateCRC(msg+args);
-			this->sendMessage(msg);
-		}
-}
-
-void conectorBusCan::cmWrInmDisplay(string ID, string args) {
-	string msg =ID+"0C";
-	int size = args.size();
-		if ( (size > 2) && (size < 13) ){
-			msg += "0"+size;
-			msg += args + this->calculateCRC(msg+args);
-			this->sendMessage(msg);
-		}
-}
-
-void conectorBusCan::cmClrDisplay(string ID) {
-	string msg =ID+"0A00";
-		msg += this->calculateCRC(msg);
-		this->sendMessage(msg);
-}
-
-void conectorBusCan::cmSaveAndRestoreDisplay(string ID, string args) {
-}
-
-void conectorBusCan::cmActiveLedBuzzer(string ID, string args) {
-	string msg =ID+"5A02";
-		if (args.size() == 4){
-			msg += args + this->calculateCRC(msg+args);
-			this->sendMessage(msg);
-		}
-}
-
-void conectorBusCan::cmSwitchLed(string ID, string args) {
-	string msg =ID+"5B02";
-		if (args.size() == 4){
-			msg += args + this->calculateCRC(msg+args);
-			this->sendMessage(msg);
-		}
-}
-
-void conectorBusCan::cmOutPort(string ID, string args) {
-}
-
-void conectorBusCan::cmEjectCard(string ID) {
-}
-
-void conectorBusCan::cmCaptureCard(string ID) {
-}
-
-void conectorBusCan::cmSetCFG(string ID, string args) {
-	string msg =ID+"4206";
-		if (args.size() == 12){
-			msg += args + this->calculateCRC(msg+args);
-			this->sendMessage(msg);
-		}
-}
-
-void conectorBusCan::cmGetCFG(string ID) {
-	string msg =ID+"4300";
-		msg += this->calculateCRC(msg);
-		this->sendMessage(msg);
-}
-
-void conectorBusCan::cmTxDigitalInput(string ID) {
-}
-
-
-*/
-
-
--- conectorSerial/conectorBusCan.h
+++ conectorSerial/conectorBusCan.h
@@ -24,8 +24,20 @@ enum codeNumberCommand{
 	cm_TestLink = 0,
 	cm_Reset =1,
 	cm_GetFirmware=2,
-	cm_SwitchRelay=82,
-	cm_SetCFG=0x42
+	cm_CloseRelay=0x51,
+	cm_SwitchRelay=0x52,
+	cm_WrDisplay=0x0B,
+	cm_WrInmDisplay=0x0C,
+	cm_ClrDisplay=0x0A,
+	cm_SaveAndRestoreDisplay=0x0D,
+	cm_ActivateLed=0x5A,
+	cm_SwitchLed=0x5B,
+	cm_OutputPort=0x64,
+	cm_EjectCard=0x59,
+	cm_CaptureCard=0x60,
+	cm_SetCFG=0x42,
+	cm_GetCFG=0x43,
+	cm_TxDigitalInput=0x53
 };
 
 class conectorBusCan {
@@ -33,32 +45,10 @@ protected: // atributos
 	map<string,string> commandsID;
 	map<string,string> commandsName;
 
+	string port;
+
 private: // Ejecución de comandos.
-	void cmTestLink(string ID="00");
-	void cmReset(string ID);
-	void cmGetFirmware(string ID);
-
-	void cmCloseRelay(string ID,string args);
-	void cmSwitchRelay(string ID,string args);
-
-	void cmWrDisplay(string ID,string args);
-	void cmWrInmDisplay(string ID,string args);
-	void cmClrDisplay(string ID);
-
-	void cmSaveAndRestoreDisplay(string ID,string args);
-	void cmActiveLedBuzzer(string ID,string args);
-	void cmSwitchLed(string ID,string args);
-
-	void cmOutPort(string ID,string args);
-	void cmEjectCard(string ID);
-	void cmCaptureCard(string ID);
-	void cmSetCFG(string ID,string args);
-	void cmGetCFG(string ID);
-
-	void cmTxDigitalInput(string ID);
-
 	bool sendMessage(string msg);
-
 	bool buildEmptyCmd(string cmd,string node);
 	bool buildArgsCmd(string cmd,string node,string args,int minArgs,int maxArgs);
 
